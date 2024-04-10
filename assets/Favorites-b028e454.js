98140b258bc94c1c4036af004d4bd05bfa2a19e0
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-04-10 08:35:30 +0000

    Deploying to gh-pages from @ NataliaMahera/Learn-Lingo@a8ba5f74e89ee8d4c53e44d7016e2160e5b39553 🚀

---
+++ assets/Favorites-b028e454.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-507c1ecf.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
---
+++ assets/Teachers-9bf2e665.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-507c1ecf.js";const s=()=>e.jsx("div",{children:"Teachers"});export{s as default};

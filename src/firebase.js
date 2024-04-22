fa4441a538bf3f43588d195439fbd079c13afceb
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-04-22 23:16:14 +0200

    fix firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -9,18 +9,19 @@ const {
   VITE_STORAGE_BACKET,
   VITE_MESSAGIN_SENDER_ID,
   VITE_API_ID,
-  VITE_DATABASE_URL,
 } = import.meta.env;
 
 const firebaseConfig = {
   apiKey: VITE_API_KEY,
   authDomain: VITE_AUTH_DOMAIN,
-  databaseURL: VITE_DATABASE_URL,
+  databaseURL:
+    'https://learnlingoauth-default-rtdb.europe-west1.firebasedatabase.app/',
   projectId: VITE_PROJECT_ID,
   storageBucket: VITE_STORAGE_BACKET,
   messagingSenderId: VITE_MESSAGIN_SENDER_ID,
   appId: VITE_API_ID,
 };
+console.log('firebaseConfig: ', firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);

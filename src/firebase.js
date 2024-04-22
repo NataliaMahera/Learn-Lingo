f662e7cb15eb7171674538d94f641b0e88f691da
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-04-22 23:22:46 +0200

    fix env

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ const {
   VITE_STORAGE_BACKET,
   VITE_MESSAGIN_SENDER_ID,
   VITE_API_ID,
+  VITE_DATABASE_URL,
 } = import.meta.env;
 
 const firebaseConfig = {
   apiKey: VITE_API_KEY,
   authDomain: VITE_AUTH_DOMAIN,
-  databaseURL:
-    'https://learnlingoauth-default-rtdb.europe-west1.firebasedatabase.app/',
+  databaseURL: VITE_DATABASE_URL,
   projectId: VITE_PROJECT_ID,
   storageBucket: VITE_STORAGE_BACKET,
   messagingSenderId: VITE_MESSAGIN_SENDER_ID,

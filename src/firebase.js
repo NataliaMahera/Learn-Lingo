59f57b6373e6c657456525035febe17bf9b3f0a4
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-04-22 23:28:23 +0200

    try to fix firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,6 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const {
-  VITE_API_KEY,
   VITE_AUTH_DOMAIN,
   VITE_PROJECT_ID,
   VITE_STORAGE_BACKET,
@@ -13,7 +12,7 @@ const {
 } = import.meta.env;
 
 const firebaseConfig = {
-  apiKey: VITE_API_KEY,
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: VITE_AUTH_DOMAIN,
   databaseURL: VITE_DATABASE_URL,
   projectId: VITE_PROJECT_ID,
@@ -21,7 +20,6 @@ const firebaseConfig = {
   messagingSenderId: VITE_MESSAGIN_SENDER_ID,
   appId: VITE_API_ID,
 };
-console.log('firebaseConfig: ', firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);

caf910ff43b2201086f9fd6a9d38690e2d488e24
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-04-23 08:10:06 +0000

    Deploying to gh-pages from @ NataliaMahera/Learn-Lingo@c428514f7052876c3be50a8f15dd1308af0c2df1 🚀

--- assets/Favorites-41a564cc.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-215644d1.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
---
+++ assets/Favorites-c50c0ba6.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-9dd4c632.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};

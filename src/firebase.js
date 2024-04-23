c428514f7052876c3be50a8f15dd1308af0c2df1
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-04-23 10:09:31 +0200

    update config databaseURL

--- src/firebase.js
+++ src/firebase.js
@@ -3,8 +3,6 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const {
- 
-  VITE_AUTH_DOMAIN,
   VITE_PROJECT_ID,
   VITE_STORAGE_BACKET,
   VITE_MESSAGIN_SENDER_ID,
@@ -14,7 +12,8 @@ const {
 
 const firebaseConfig = {
   apiKey: 'AIzaSyB27k5q4hCUt97rf8cKwbDVqFgq7wjS9o4',
-  authDomain: VITE_AUTH_DOMAIN,
+  authDomain:
+    'https://learnlingoauth-default-rtdb.europe-west1.firebasedatabase.app/',
   databaseURL: VITE_DATABASE_URL,
   projectId: VITE_PROJECT_ID,
   storageBucket: VITE_STORAGE_BACKET,

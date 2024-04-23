eb31ff5f012e8f68d9b3af1cb90e620a42667285
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-04-23 10:12:43 +0200

    fix errors with databaseUrl

--- src/firebase.js
+++ src/firebase.js
@@ -3,18 +3,18 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const {
+  VITE_AUTH_DOMAIN,
   VITE_PROJECT_ID,
   VITE_STORAGE_BACKET,
   VITE_MESSAGIN_SENDER_ID,
   VITE_API_ID,
-  VITE_DATABASE_URL,
 } = import.meta.env;
 
 const firebaseConfig = {
   apiKey: 'AIzaSyB27k5q4hCUt97rf8cKwbDVqFgq7wjS9o4',
-  authDomain:
+  authDomain: VITE_AUTH_DOMAIN,
+  databaseURL:
     'https://learnlingoauth-default-rtdb.europe-west1.firebasedatabase.app/',
-  databaseURL: VITE_DATABASE_URL,
   projectId: VITE_PROJECT_ID,
   storageBucket: VITE_STORAGE_BACKET,
   messagingSenderId: VITE_MESSAGIN_SENDER_ID,

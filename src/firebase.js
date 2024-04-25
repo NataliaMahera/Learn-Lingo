8d106a0f064a227a47ac41549a07b0919760a292
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-04-25 14:52:12 +0200

    add env example file

--- src/firebase.js
+++ src/firebase.js
@@ -3,24 +3,24 @@ import { getAuth } from 'firebase/auth';
 
 import { getDatabase } from 'firebase/database';
 
-const {
-  VITE_API_KEY,
-  VITE_AUTH_DOMAIN,
-  VITE_DATABASE_URL,
-  VITE_PROJECT_ID,
-  VITE_STORAGE_BACKET,
-  VITE_MESSAGIN_SENDER_ID,
-  VITE_API_ID,
-} = import.meta.env;
+// const {
+//   VITE_API_KEY,
+//   VITE_AUTH_DOMAIN,
+//   VITE_DATABASE_URL,
+//   VITE_PROJECT_ID,
+//   VITE_STORAGE_BACKET,
+//   VITE_MESSAGIN_SENDER_ID,
+//   VITE_API_ID,
+// } = import.meta.env;
 
 const firebaseConfig = {
-  apiKey: VITE_API_KEY,
-  authDomain: VITE_AUTH_DOMAIN,
-  databaseURL: VITE_DATABASE_URL,
-  projectId: VITE_PROJECT_ID,
-  storageBucket: VITE_STORAGE_BACKET,
-  messagingSenderId: VITE_MESSAGIN_SENDER_ID,
-  appId: VITE_API_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BACKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGIN_SENDER_ID,
+  appId: import.meta.env.VITE_API_ID,
 };
 
 console.log('firebaseConfig', firebaseConfig);
